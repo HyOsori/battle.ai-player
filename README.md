dd89542a481fea035d376ca80461eba69fee16e2
junsuLime <vfv0121@gmail.com>
2017-07-20 20:47:18 +0900

    update battle.ai player

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
-#Battle AI 시즌 1
-<a href="http://cafe.naver.com/osoribattleai">대회 카페</a>
+# Battle AI
+
+<a href="http://cafe.naver.com/osoribattleai">대회</a>
---
+++ you.py
@@ -0,0 +1,7 @@
+from base.player import play
+from customlogic import CustomOMOKLogic
+
+
+if __name__ == "__main__":
+    logic = CustomOMOKLogic()
+    play(logic)
